ae658f63e1a79ce5de97b04886663e90a553db49
NatalyCreates <nataly@natalycreates.com>
2017-04-23 02:25:52 +0300

    exit to menu

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
@@ -28,6 +29,9 @@ public class GameManager : MonoBehaviour {
 	}
 	
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("menu");
+        }
 	}
 }
